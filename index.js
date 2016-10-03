1cff1b03411b5cc91aff2b321de69f01a7f24390
Livio Brunner <livio.brunner.lb1@roche.com>
2016-10-03 16:54:12 +0200

    create simple http server using expressjs

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var tspReader = require('./tsp-reader/tsp-reader');
+var tspReader = require('./tsp-reader/tsp-reader'),
+    express = require('express'),
+    app = express(),
+    http = require('http').createServer(app);
+    
+app.set("ipaddr", "127.0.0.1");
 
-tspReader
-    .subscribe(function(data){
-        console.log(data);
-    })
-    .watch();
+app.set("port", 3000);
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/public/index.html");
+});
+
+http.listen(app.get("port"), app.get("ipaddr"), function() {
+  console.log("Server up and running. Go to http://" + app.get("ipaddr") + ":" + app.get("port"));
+});
