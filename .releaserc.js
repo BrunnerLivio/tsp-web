70ac57600ac42444a2f82148357ad6c3e8dc6326
Livio Brunner <livio.brunner.lb1@gmail.com>
2023-05-19 17:24:56 +0200

    chore: remove @semantic-release/git

--- .releaserc.js
+++ .releaserc.js
@@ -13,7 +13,6 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    "@semantic-release/git",
     [
       "@semantic-release-plus/docker",
       {
