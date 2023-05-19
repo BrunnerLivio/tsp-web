828c608020f127f16707a4a6518f32afe659dc06
Livio Brunner <livio.brunner.lb1@gmail.com>
2023-05-19 17:10:52 +0200

    fix: npm release process

---
+++ .releaserc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  release: {
+    branches: [
+      "+([0-9])?(.{+([0-9]),x}).x",
+      "master",
+      "next",
+      "next-major",
+      { name: "beta", prerelease: true },
+      { name: "alpha", prerelease: true },
+    ],
+  },
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    [
+      "@semantic-release-plus/docker",
+      {
+        name: process.env.IMAGE_NAME,
+        registry: process.env.REGISTRY,
+      },
+    ],
+  ],
+  publish: [
+    {
+      path: "@semantic-release/github",
+      assets: [
+        {
+          path: "tsp-web*.deb",
+          label: "Debian Package",
+        },
+      ],
+    },
+  ],
+};
