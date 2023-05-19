edd2acb968c85480ee1b1582b3a7d84964c0ccc8
Livio Brunner <livio.brunner.lb1@gmail.com>
2023-05-19 18:14:24 +0200

    chore: cleanup releaserc config

--- .releaserc.js
+++ .releaserc.js
@@ -1,35 +1,33 @@
 module.exports = {
-  release: {
-    branches: [
-      "+([0-9])?(.{+([0-9]),x}).x",
-      "master",
-      "next",
-      "next-major",
-      { name: "beta", prerelease: true },
-      { name: "alpha", prerelease: true },
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "master",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    [
+      "@semantic-release-plus/docker",
+      {
+        name: process.env.IMAGE_NAME,
+        registry: process.env.REGISTRY,
+      },
     ],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/npm",
-      [
-        "@semantic-release-plus/docker",
-        {
-          name: process.env.IMAGE_NAME,
-          registry: process.env.REGISTRY,
-        },
-      ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "tsp-web*.deb",
+            label: "Debian Package",
+          },
+        ],
+      },
     ],
-  },
-  publish: [
-    {
-      path: "@semantic-release/github",
-      assets: [
-        {
-          path: "tsp-web*.deb",
-          label: "Debian Package",
-        },
-      ],
-    },
   ],
 };
